208c4480f11037a2dd5b6d3e81d7b117b2919457
José Vitor <jose-vitor-7@hotmail.com>
2024-02-02 02:16:31 -0300

    Design System Components - box

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,19 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Box, BoxProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Surfaces/Box',
+  component: Box,
+
+  args: {
+    children: (
+      <>
+        <span>Testando o elemento box</span>
+      </>
+    ),
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {
+  args: {},
+}
