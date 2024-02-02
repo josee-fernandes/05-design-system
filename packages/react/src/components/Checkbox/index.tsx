78dee778cd8341d88f134f868e37e3a062f8d14c
José Vitor <jose-vitor-7@hotmail.com>
2024-02-02 04:02:07 -0300

    Design System Components - checkbox

---
+++ packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,15 @@
+import { ComponentProps } from 'react'
+import { Check } from 'phosphor-react'
+import { CheckBoxContainer, CheckboxIndicator } from './styles'
+
+export type CheckboxProps = ComponentProps<typeof CheckBoxContainer>
+
+export function Checkbox(props: CheckboxProps) {
+  return (
+    <CheckBoxContainer {...props}>
+      <CheckboxIndicator asChild>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckBoxContainer>
+  )
+}
